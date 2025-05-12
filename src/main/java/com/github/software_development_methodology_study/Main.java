dabed7c72d7b8f8ffbcf6f336a2c068dd670f37b
can019 <jys01012@gmail.com>
2025-05-12 20:47:19 +0900

    chore: initial commit  Changes to be committed: 	modified:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/com/github/software_development_methodology_study/Main.java

---
+++ src/main/java/com/github/software_development_methodology_study/Main.java
@@ -0,0 +1,7 @@
+package com.github.software_development_methodology_study;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
