513ca2ce3ed746ac1cf28b75df8b991e459a8b9f
can019 <jys01012@gmail.com>
2025-05-16 15:38:17 +0900

    chore: Init guilayer  Changes to be committed: 	new file:   src/main/java/com/github/software_development_methodology_study/core/layer/GuiLayer.java

---
+++ src/main/java/com/github/software_development_methodology_study/core/layer/GuiLayer.java
@@ -0,0 +1,16 @@
+package com.github.software_development_methodology_study.core.layer;
+
+import com.github.software_development_methodology_study.core.data.chunk.Chunk;
+import com.github.software_development_methodology_study.core.data.chunk.header.EmptyHeader;
+
+public class GuiLayer extends Layer<EmptyHeader>{
+    @Override
+    public void receive(Chunk chunk) {
+
+    }
+
+    @Override
+    public void send(Chunk chunk) {
+
+    }
+}
