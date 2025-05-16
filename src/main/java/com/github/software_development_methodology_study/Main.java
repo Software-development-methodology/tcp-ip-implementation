5a87761b96da08ed661917436606215e808ce4a7
Yousung Jung <jys01012@gmail.com>
2025-05-16 15:34:23 +0900

    Data 추상화 및 abstract Layer 작성 (#17)
    * chore: initial commit
     Changes to be committed:
    	modified:   .gitignore
    	new file:   build.gradle
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    	new file:   src/main/java/com/github/software_development_methodology_study/Main.java
    
    * feat: Chunk, Header, Payload 클래스 초안 작성
    - iv에 집중
    - iv 변경에 대해 고민 히지 않은 상태,  getter/setter는 모두 열어 둠
    - Generic 고려하지 않음
     Changes to be committed:
    	new file:   src/main/java/com/github/software_development_methodology_study/core/data/chunk/Chunk.java
    	new file:   src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/Header.java
    	new file:   src/main/java/com/github/software_development_methodology_study/core/data/chunk/payload/Payload.java
    
    * feat: 고정된 크기임을 나타내는 interface
    - 크기가 정해져있는 데이터에서 implements
    - Header class에 implements
     Changes to be committed:
      modified:   src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/Header.java
      new file:   src/main/java/com/github/software_development_methodology_study/core/data/contract/FixedSize.java
    
    * fix: Header 생성자에 필요 없는 파라미터가 추가된 버그
    - FixedSize interface 분리 전 Header 클래스에서 자체적으로 크기를 정하기 위해 만들었던 레거시
    
    * feat: Layer 클래스 작성
    - Header 형변환 코드를 작성하지 않도록 제네릭으로 작성
    
    * test: Layer의 receive가 호출되었을 때 형변환 없이 타입이 변경되는 것 확인
    - TestLayer와 TestHeader를 작성하여 테스트
    
    * chore: Main 메서드 비움
    
    * test: Layer간 receive 체이닝 시 형변환 없이 타입 변경 확인
    
    * docs: 자바독 작성
    - method들은 적지 않음
    
    * feat: Cast 메서드 작성
    
    * fix: Layer에 Chunk, payload 필드 삭제
    
    * refactor: Header에서 fixedsize implemenets 하지 않도록 변경
    - TCP/IP는 가변길이기 때문에 Header 길이가 고정적이지 않음
    - 고정적인 길이의 Header만 이를 implements 하도록 변경
    
    * chore: Cast 메서드 삭제
    - Layer 메서드에서 Chunk<Hedaer>가 아닌 T를 사용하면 됨
    
    * chore: 사용하지 않는 import 제거
     Changes to be committed:
    	modified:   src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/Header.java
    
    * chore: Test 클래스에서 사용하지 않는 메서드 제거

---
+++ src/main/java/com/github/software_development_methodology_study/Main.java
@@ -0,0 +1,6 @@
+package com.github.software_development_methodology_study;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/github/software_development_methodology_study/core/layer/TestHeader.java
@@ -0,0 +1,6 @@
+package com.github.software_development_methodology_study.core.layer;
+
+import com.github.software_development_methodology_study.core.data.chunk.header.Header;
+
+public class TestHeader extends Header {
+}
