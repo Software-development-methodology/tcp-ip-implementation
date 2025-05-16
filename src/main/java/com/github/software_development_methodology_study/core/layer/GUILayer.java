ed6d9fd712638aade1e637b25ee44157f562e620
can019 <jys01012@gmail.com>
2025-05-16 17:46:49 +0900

    chore: Init GUI Layer class  Changes to be committed: 	new file:   src/main/java/com/github/software_development_methodology_study/core/layer/GUILayer.java

---
+++ src/main/java/com/github/software_development_methodology_study/core/layer/GUILayer.java
@@ -0,0 +1,17 @@
+package com.github.software_development_methodology_study.core.layer;
+
+import com.github.software_development_methodology_study.core.data.chunk.Chunk;
+import com.github.software_development_methodology_study.core.data.chunk.header.EmptyHeader;
+import com.github.software_development_methodology_study.core.data.chunk.header.Header;
+
+public class GUILayer extends Layer<EmptyHeader> {
+    @Override
+    public void receive(Chunk<Header> chunk) {
+
+    }
+
+    @Override
+    public void send(Chunk<Header> chunk) {
+
+    }
+}
