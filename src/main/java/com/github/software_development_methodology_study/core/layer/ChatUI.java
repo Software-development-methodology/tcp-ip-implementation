7ea1f7e3fdce987bc60b7ee99c5969ae4ab8a3e2
can019 <jys01012@gmail.com>
2025-05-18 21:17:54 +0900

    chore: 사용하지 않는 import 제거  Changes to be committed: 	modified:   src/main/java/com/github/software_development_methodology_study/core/layer/ChatUI.java

--- src/main/java/com/github/software_development_methodology_study/core/layer/ChatUI.java
+++ src/main/java/com/github/software_development_methodology_study/core/layer/ChatUI.java
@@ -1,6 +1,5 @@
 package com.github.software_development_methodology_study.core.layer;
 
-import com.github.software_development_methodology_study.common.util.DataType;
 import com.github.software_development_methodology_study.core.data.chunk.Chunk;
 import com.github.software_development_methodology_study.core.data.chunk.header.EmptyHeader;
 import com.github.software_development_methodology_study.core.data.chunk.header.Header;
