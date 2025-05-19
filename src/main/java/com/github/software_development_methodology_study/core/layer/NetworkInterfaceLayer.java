a099672ccc768f5415267a1b3a63a9cd2e502bc8
Seungmin <rnsdls1997@gmail.com>
2025-05-19 18:20:56 +0900

    feat: NILayer 틀잡기

---
+++ src/main/java/com/github/software_development_methodology_study/core/layer/NetworkInterfaceLayer.java
@@ -0,0 +1,24 @@
+package com.github.software_development_methodology_study.core.layer;
+
+import com.github.software_development_methodology_study.core.data.chunk.Chunk;
+import com.github.software_development_methodology_study.core.data.chunk.header.EmptyHeader;
+
+public class NetworkInterfaceLayer extends Layer<EmptyHeader> implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void receive(Chunk chunk) {
+
+    }
+
+    @Override
+    public void send(Chunk chunk) {
+
+    }
+
+
+}
