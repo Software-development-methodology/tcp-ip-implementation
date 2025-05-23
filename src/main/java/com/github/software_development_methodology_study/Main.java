96bb1d73874a5b01f2a4682cdca5c9dca60b1a28
Seungmin <rnsdls1997@gmail.com>
2025-05-23 14:16:51 +0900

    refactor: ni-layer 수정

--- src/main/java/com/github/software_development_methodology_study/Main.java
+++ src/main/java/com/github/software_development_methodology_study/Main.java
@@ -29,10 +29,7 @@ public class Main {
 
 
 
-        List<PcapNetworkInterface> interfaces1 = Pcaps.findAllDevs();
-        for (PcapNetworkInterface nif : interfaces1) {
-            System.out.println(nif.getName() + " : " + nif.getDescription());
-        }
+
         // NIF 가져오기
 //        PcapNetworkInterface nif = Pcaps.getDevByName("\\Device\\NPF_{6F60B71D-1C9E-4C66-97E6-FBA81EBD2E6E}");
         PcapNetworkInterface nif = Pcaps.getDevByName("\\Device\\NPF_{83D8ABE6-6CA7-45BA-A361-335258177916}");
@@ -45,10 +42,13 @@ public class Main {
         // 캡처 핸들러 열기
         PcapHandle handle = nif.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, 10000);
 
-        // 패킷 캡처
         Packet packet = handle.getNextPacketEx();
-        System.out.println(packet);
-
+        // 패킷 캡처
+        int i = 100;
+        while (i-->1) {
+            packet = handle.getNextPacketEx();
+            System.out.println(packet);
+        }
 
 // 바이트 배열 형태의 로우 패킷 데이터
         byte[] rawData = packet.getRawData();
