66a927b530217bbb014aa34a87c0f302c329d1af
can019 <jys01012@gmail.com>
2025-05-28 10:46:38 +0900

    fix: FrameHeader 빌더 수동으로 생성 - 자식에서 부모 필드를 생성 (super 호출)하는 경우 `@Builder`사용 시 오류 - 수동 생성 또는 `experimental` 패키지에 있는 롬복을 사용해야함 - `experimental`이기 때문에 수동 생성 선택

--- src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/FrameHeader.java
+++ src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/FrameHeader.java
@@ -1,13 +1,11 @@
 package com.github.software_development_methodology_study.core.data.chunk.header;
 
-import lombok.Builder;
-
-@Builder
 public class FrameHeader extends Header{
     private final Byte[] destinationMac;
     private final Byte[] srcMac;
     private final Byte[] type;
     private final Byte[] fcs;
+
 
     public FrameHeader(Byte[] rawHeader, Byte[] destinationMac, Byte[] srcMac, Byte[] type, Byte[] fcs) {
         super(rawHeader);
@@ -30,5 +28,46 @@ public class FrameHeader extends Header{
     }
     public Byte[] getFcs() {
         return fcs;
+    }
+
+    public static class FrameHeaderBuilder {
+        private Byte[] rawHeader;
+        private Byte[] destinationMac;
+        private Byte[] srcMac;
+        private Byte[] type;
+        private Byte[] fcs;
+
+        public FrameHeaderBuilder rawHeader(Byte[] rawHeader) {
+            this.rawHeader = rawHeader;
+            return this;
+        }
+
+        public FrameHeaderBuilder destinationMac(Byte[] destinationMac) {
+            this.destinationMac = destinationMac;
+            return this;
+        }
+
+        public FrameHeaderBuilder srcMac(Byte[] srcMac) {
+            this.srcMac = srcMac;
+            return this;
+        }
+
+        public FrameHeaderBuilder type(Byte[] type) {
+            this.type = type;
+            return this;
+        }
+
+        public FrameHeaderBuilder fcs(Byte[] fcs) {
+            this.fcs = fcs;
+            return this;
+        }
+
+        public FrameHeader build() {
+            return new FrameHeader(rawHeader, destinationMac, srcMac, type, fcs);
+        }
+    }
+
+    public static FrameHeaderBuilder builder() {
+        return new FrameHeaderBuilder();
     }
 }
