7b945d2e4fd6aa622fbbe3f52010ad2f34117d55
kdhur <1kdhur2@gmail.com>
2025-05-28 11:50:38 +0900

    feat: EthernetLayer 필요 메서드 선언부 추가 MAC 주소 추가 메서드 MAC 주소 비교 메서드 데이터 사이즈 검증 메서드

--- src/main/java/com/github/software_development_methodology_study/core/layer/EthernetLayer.java
+++ src/main/java/com/github/software_development_methodology_study/core/layer/EthernetLayer.java
@@ -1,4 +1,28 @@
 package com.github.software_development_methodology_study.core.layer;
 
-public class EthernetLayer {
+import com.github.software_development_methodology_study.core.data.chunk.Chunk;
+import com.github.software_development_methodology_study.core.data.chunk.payload.Payload;
+
+public class EthernetLayer extends Layer {
+    @Override
+    public void receive(Chunk chunk) {
+
+    }
+
+    @Override
+    public void send(Chunk chunk) {
+
+    }
+
+    private Byte[] addMacAddress(Byte[] macAddress) {
+        return null;
+    }
+
+    private boolean compareMacAddress() {
+        return false;
+    }
+
+    private boolean validatePayloadSize(Payload payload) {
+        return false;
+    }
 }
