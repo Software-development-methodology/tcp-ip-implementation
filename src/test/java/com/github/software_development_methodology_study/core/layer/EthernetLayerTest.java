7e788b729dcd4f49ba9175042b11bdfc244688e2
can019 <jys01012@gmail.com>
2025-05-29 16:18:24 +0900

    test: 이더넷 레이어 Receive 수행 전, 후 chunk 크기 테스트 - Receive 호출 시 chunk의 크기, receive 수행 후 chunk의 크기 테스트 - FrameHeader는 14byte (fixed), payload는 46byte이상 1500이하

--- src/test/java/com/github/software_development_methodology_study/core/layer/EthernetLayerTest.java
+++ src/test/java/com/github/software_development_methodology_study/core/layer/EthernetLayerTest.java
@@ -25,4 +25,77 @@ class EthernetLayerTest {
 
         assertThrows(IllegalArgumentException.class, () -> ethernetLayer.send(chunk));
     }
+
+    @Test
+    void receive_에서_헤더_제거_후_payload_크기가_46byte_미만이면_illigalArgumentException이_발생한다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[30];
+        chunk.setPayload(new Payload(bytes));
+
+        assertThrows(IllegalArgumentException.class, ()-> ethernetLayer.receive(chunk));
+    }
+
+    @Test
+    void receive_에서_헤더_제거_후_payload_크기가_정확히_46byte면_예외가_발생하지_않는다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[46]; // 경계값
+        chunk.setPayload(new Payload(bytes));
+
+        assertDoesNotThrow(() -> ethernetLayer.receive(chunk));
+    }
+
+    @Test
+    void receive_에서_헤더_제거_후_payload_크기가_1500byte_초과하면_illigalArgumentException이_발생한다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[1501];
+        chunk.setPayload(new Payload(bytes));
+
+        assertThrows(IllegalArgumentException.class, ()-> ethernetLayer.receive(chunk));
+    }
+
+    @Test
+    void receive_에서_헤더_제거_후_payload_크기가_정확히_1500byte면_예외가_발생하지_않는다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[1500]; // 경계값
+        chunk.setPayload(new Payload(bytes));
+
+        assertDoesNotThrow(() -> ethernetLayer.receive(chunk));
+    }
+
+    @Test
+    void receive_호출_시_Chunk_크기가_60byte를_미만이면_illigalArgumentException이_발생한다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[57];
+        chunk.setPayload(new Payload(bytes));
+
+        assertThrows(IllegalArgumentException.class, ()-> ethernetLayer.receive(chunk));
+    }
+
+    @Test
+    void receive_호출_시_Chunk_크기가_정확히_60byte면_illigalArgumentException이_발생하지_않는다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[60]; // 경계값
+        chunk.setPayload(new Payload(bytes));
+
+        assertDoesNotThrow(()-> ethernetLayer.receive(chunk));
+    }
+
+
+    @Test
+    void receive_호출_시_Chunk_크기가_1514byte_초과하면_illigalArgumentException이_발생한다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[1518];
+        chunk.setPayload(new Payload(bytes));
+
+        assertThrows(IllegalArgumentException.class, ()-> ethernetLayer.receive(chunk));
+    }
+
+    @Test
+    void receive_호출_시_Chunk_크기가_정확히_1514byte면_illigalArgumentException이_발생하지_않는다() {
+        Chunk<EmptyHeader> chunk = new Chunk<>();
+        Byte[] bytes = new Byte[1514]; // 경계값
+        chunk.setPayload(new Payload(bytes));
+
+        assertDoesNotThrow(()-> ethernetLayer.receive(chunk));
+    }
 }
