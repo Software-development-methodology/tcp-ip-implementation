60e8bb848fc8d0da75f4b8dc620ff2cf1b9b56ec
daamont <daamont01@gmail.com>
2025-05-29 15:56:23 +0900

    fix: FrameHeader 필드 수정 - nic에서 FCS, PREMBLE은 자동으로 붙여줌

--- src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/FrameHeader.java
+++ src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/FrameHeader.java
@@ -1,20 +1,16 @@
 package com.github.software_development_methodology_study.core.data.chunk.header;
 
 public class FrameHeader extends Header{
-    private final Byte[] preamble;
     private final Byte[] destinationMac;
     private final Byte[] srcMac;
     private final Byte[] type;
-    private final Byte[] fcs;
 
 
-    public FrameHeader(Byte[] rawHeader, Byte[] preamble, Byte[] destinationMac, Byte[] srcMac, Byte[] type, Byte[] fcs) {
+    public FrameHeader(Byte[] rawHeader, Byte[] destinationMac, Byte[] srcMac, Byte[] type) {
         super(rawHeader);
-        this.preamble = preamble;
         this.destinationMac = destinationMac;
         this.srcMac = srcMac;
         this.type = type;
-        this.fcs = fcs;
     }
 
     public Byte[] getDestinationMac() {
@@ -28,17 +24,12 @@ public class FrameHeader extends Header{
     public Byte[] getType() {
         return type;
     }
-    public Byte[] getFcs() {
-        return fcs;
-    }
 
     public static class FrameHeaderBuilder {
         private Byte[] rawHeader;
         private Byte[] destinationMac;
         private Byte[] srcMac;
         private Byte[] type;
-        private Byte[] fcs;
-        private Byte[] preamble;
 
         public FrameHeaderBuilder rawHeader(Byte[] rawHeader) {
             this.rawHeader = rawHeader;
@@ -60,18 +51,8 @@ public class FrameHeader extends Header{
             return this;
         }
 
-        public FrameHeaderBuilder fcs(Byte[] fcs) {
-            this.fcs = fcs;
-            return this;
-        }
-
-        public FrameHeaderBuilder preamble(Byte[] preamble) {
-            this.preamble = preamble;
-            return this;
-        }
-
         public FrameHeader build() {
-            return new FrameHeader(rawHeader, preamble, destinationMac, srcMac, type, fcs);
+            return new FrameHeader(rawHeader, destinationMac, srcMac, type);
         }
     }
 
@@ -80,11 +61,9 @@ public class FrameHeader extends Header{
     }
 
     private enum HeaderField{
-        PREAMBLE(0, 8), // 시작 index, 길이
         DESTINATION_MAC(8, 6),
         SRC_MAC(14, 6),
-        TYPE(24, 2),
-        FCS(-4, 4);
+        TYPE(24, 2);
 
         private final int startIndex;
         private final int length;
@@ -98,8 +77,8 @@ public class FrameHeader extends Header{
             return startIndex >= 0 ? startIndex : (totalLength + startIndex);
         }
 
-        public int getEndIndex(int totalLength) {
-            return (getStartIndex(totalLength) + length) -1;
+        public int getEndIndex() {
+            return (startIndex + length) -1;
         }
 
         public int getLength() {
