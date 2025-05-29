6fba29f1c6c286bdc432aad5f811fd69a67ed166
can019 <jys01012@gmail.com>
2025-05-29 18:37:31 +0900

    chore: init

---
+++ spike/issue40-nic-set/yousung-jung/NicSet/src/main/java/com/github/software_development_methodology_study/common/util/TypeConverter.java
@@ -0,0 +1,32 @@
+package com.github.software_development_methodology_study.common.util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public final class TypeConverter {
+    private TypeConverter(){}
+
+    public static Byte[] StringToByteArray(String str) {
+        byte[] byteArray = str.getBytes(StandardCharsets.UTF_8); // or Charset.forName("UTF-8")
+        Byte[] byteObjectArray = new Byte[byteArray.length];
+
+        for (int i = 0; i < byteArray.length; i++) {
+            byteObjectArray[i] = byteArray[i]; // auto-boxing
+        }
+        return byteObjectArray;
+    }
+
+    public static String ByteArrayToString(Byte[] bytes) throws IllegalArgumentException {
+        if (Objects.isNull(bytes)) throw new IllegalArgumentException("Input Byte array cannot be null");
+
+        byte[] primitiveBytes = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == null) {
+                throw new IllegalArgumentException("Byte array contains null at index " + i);
+            }
+            primitiveBytes[i] = bytes[i];
+        }
+
+        return new String(primitiveBytes, StandardCharsets.UTF_8);
+    }
+}
---
+++ spike/issue40-nic-set/yousung-jung/NicSet/src/main/java/com/github/software_development_methodology_study/core/data/chunk/header/Header.java
@@ -0,0 +1,28 @@
+package com.github.software_development_methodology_study.core.data.chunk.header;
+
+
+/**
+ * 네트워크 전송 계층에서 사용되는 공통 헤더 추상 클래스입니다.
+ * <p>
+ * 모든 구체적인 헤더 클래스는 이 클래스를 확장하여 고정 크기 바이트 배열을 기반으로 구성됩니다.
+ *
+ * @see com.github.software_development_methodology_study.core.data.chunk.header.EmptyHeader
+ * @since 0.0.1
+ * @version 1.0
+ * @author jeong-yuseong
+ */
+public abstract class Header {
+    protected final Byte[] bytes;
+
+    protected Header() {
+        this(new Byte[0]);
+    }
+
+    protected Header(Byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public final Byte[] getBytes() {
+        return bytes;
+    }
+}
