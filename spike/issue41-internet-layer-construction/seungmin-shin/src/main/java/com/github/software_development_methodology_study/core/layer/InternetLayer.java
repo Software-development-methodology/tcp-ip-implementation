acfeb4e3c6b6814006d6c5879777d52f2b49f0a5
Seungmin <rnsdls1997@gmail.com>
2025-05-31 17:34:39 +0900

    feat: fragmentBuffer 저장 기능 추가

--- spike/issue41-internet-layer-construction/seungmin-shin/src/main/java/com/github/software_development_methodology_study/core/layer/InternetLayer.java
+++ spike/issue41-internet-layer-construction/seungmin-shin/src/main/java/com/github/software_development_methodology_study/core/layer/InternetLayer.java
@@ -10,6 +10,7 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 
@@ -18,25 +19,48 @@ public class InternetLayer extends Layer<PacketHeader> {
     /**
      * 패킷 식별용 클래스 <br>
      * 패킷은 srcIp, dstIp, protocol, identification으로 식별한다. <br>
+     * 필드들은 Byte[] 로 저장 <br>
      * fragmentBuffer에 [FragmentKey, [Offset, Payload]] 형태로 저장
      */
     private class FragmentKey {
-        private final String srcIp;
-        private final String dstIp;
-        private final int protocol;
-        private final int identification;
+        private final Byte[] srcIp;
+        private final Byte[] dstIp;
+        private final Byte[] protocol;
+        private final Byte[] identification;
 
-        public FragmentKey(String srcIp, String dstIp, int protocol, int identification) {
+        public FragmentKey(Byte[] srcIp, Byte[] dstIp, Byte[] protocol, Byte[] identification) {
             this.srcIp = srcIp;
             this.dstIp = dstIp;
             this.protocol = protocol;
             this.identification = identification;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this ==  obj) return true;
+            if (obj == null || this.getClass() != obj.getClass()) return false;
+            FragmentKey key = (FragmentKey) obj;
+            return (Arrays.equals(this.srcIp, key.srcIp)
+                    && Arrays.equals(this.dstIp, key.dstIp)
+                    && Arrays.equals(this.protocol, key.protocol)
+                    && Arrays.equals(this.identification, key.identification));
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(
+                    Arrays.hashCode(srcIp),
+                    Arrays.hashCode(dstIp),
+                    Arrays.hashCode(protocol),
+                    Arrays.hashCode(identification)
+            );
         }
     }
 
     /**
      * 들어온 패킷들을 저장하는 Map
      * [FragmentKey, [Offset, Payload]] 형태로 저장
+     * Offset은 Map에서 꺼낼 때 편의를 위해 저장할 때 Integer로 저장
      */
     private final ConcurrentHashMap<FragmentKey, ConcurrentHashMap<Integer, Byte[]>> fragmentBuffer = new ConcurrentHashMap<>();
 
@@ -48,8 +72,10 @@ public class InternetLayer extends Layer<PacketHeader> {
 
         PacketHeader packetHeader = new PacketHeader();
 
+        // MEETING: 더 좋은 방법이 있는지 -> 지금 방법이 좋은지
         extractChunkAndSetNewHeader(chunk, packetHeader);
 
+        // FIXME: Github Discussion 참고
         // 목적지 아이피 주소 확인
         if(!isLocalIPAddress(packetHeader.getDestination_Address()))
             return;
@@ -61,12 +87,13 @@ public class InternetLayer extends Layer<PacketHeader> {
 //        Protocol protocol = getProtocol(null);
 
         // TODO: fragmentBuffer에 헤더 넣기..
+        // fragmentBuffer에 fragment 저장
+        storeFragmentToFragmentBuffer(packetHeader, chunk.getPayload());
 
         // TODO: isLastFragment 매개변수 수정,
         //  getFragmentKey 메서드 작성,
         //  mergeFragments 매개변수 수정
         if(isLastFragment(null)) {
-//            FragmentKey fragmentKey = getFragmentKey();
             Byte[] mergedFragment = mergeFragments(null);
             chunk.setPayload(new Payload(mergedFragment));
             upperLayer.receive(chunk);
@@ -178,7 +205,7 @@ public class InternetLayer extends Layer<PacketHeader> {
         chunk.setHeader(packetHeader); //PacketHeader
     }
 
-    // TODO: 아이피 확인 메서드 구현
+    // FIXME: 아이피 확인 메서드 구현
     private boolean isLocalIPAddress(Byte[] ipAddress) {
         try {
             byte[] localIp = InetAddress.getLocalHost().getAddress();
@@ -199,6 +226,36 @@ public class InternetLayer extends Layer<PacketHeader> {
         // More Fragments
         return true;
     }
+
+    /**
+     * fragment를 fragmentBuffer에 저장하는 메서드 <br>
+     * 기존에 fragmentKey가 있는지 확인 후 없으면 생성 <br>
+     * InternetLayer의 Header로 fragmentKey를 생성한다. <br>
+     * @param packetHeader InternetLayer 헤더
+     * @param payload InternetLayer 페이로드
+     */
+    private void storeFragmentToFragmentBuffer(PacketHeader packetHeader, Payload payload) {
+        // 들어온 패킷 정보로 key 생성
+        FragmentKey fragmentKey = new FragmentKey(
+                packetHeader.getSource_Address(),
+                packetHeader.getDestination_Address(),
+                packetHeader.getProtocol(),
+                packetHeader.getIdentification());
+
+        if(fragmentBuffer.containsKey(fragmentKey)) {
+            // 기존에 key가 있으면 기존 key에 <offset, payload> 저장
+            fragmentBuffer.get(fragmentKey).put(byteArrayToInt(packetHeader.getFragment_Offset()), payload.getBytes());
+        } else {
+            // 새로운 key 생성 후 <offset, payload> 저장
+            ConcurrentHashMap<Integer, Byte[]> newMap = new ConcurrentHashMap<>();
+            newMap.put(byteArrayToInt(packetHeader.getFragment_Offset()), payload.getBytes());
+            fragmentBuffer.put(fragmentKey, newMap);
+        }
+
+    }
+
+
+
     /**
      * 해당 패킷 키와 같은 종류의 패킷들을 합침
      * @param fragmentKey 패킷 식별키
@@ -213,5 +270,9 @@ public class InternetLayer extends Layer<PacketHeader> {
             fragmentList.addAll(Arrays.asList(fragmentMap.get(i)));
 
         return fragmentList.toArray(new Byte[0]);
+    }
+
+    private int byteArrayToInt(Byte[] byteArray) {
+        return Byte.toUnsignedInt(byteArray[0]);
     }
 }
