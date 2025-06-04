17f7e593d0dfd29461720b090ca6deb0fa606c7e
kdhur <1kdhur2@gmail.com>
2025-06-04 21:24:00 +0900

    feat: ethernet layer business logic 구현 - 목적지 주소와 호스트 주소 비교 - 헤더 추출 - 타입 검증 안함

--- src/main/java/com/github/software_development_methodology_study/core/layer/EthernetLayer.java
+++ src/main/java/com/github/software_development_methodology_study/core/layer/EthernetLayer.java
@@ -5,16 +5,34 @@ import com.github.software_development_methodology_study.core.data.chunk.header.
 import com.github.software_development_methodology_study.core.data.chunk.header.FrameHeader;
 import com.github.software_development_methodology_study.core.data.chunk.header.Header;
 import com.github.software_development_methodology_study.core.data.chunk.payload.Payload;
+import lombok.extern.slf4j.Slf4j;
 import org.pcap4j.core.PcapNetworkInterface;
 
-import static com.github.software_development_methodology_study.common.util.TypeConverter.StringToByteArray;
+import java.util.Arrays;
 
+import static com.github.software_development_methodology_study.common.util.TypeConverter.toObject;
+import static com.github.software_development_methodology_study.core.data.chunk.header.FrameHeaderField.*;
+
+@Slf4j
 public class EthernetLayer extends Layer<FrameHeader> {
     @Override
     public boolean receive(Chunk<EmptyHeader> chunk, PcapNetworkInterface nic) {
-        checkValidFrameSize(chunk);
-        // @TODO:: FrameHeader 검증 및 제거
-        checkValidFrameHeaderLessSize(chunk);
+        // framing
+        if(isInvalidFrameSize(chunk)) {
+            return false;
+        }
+        // header 추출
+        FrameHeader frameHeader = extractHeader(chunk.getPayload());
+
+        // 목적지 주소랑 호스트 주소 비교
+        if (Arrays.equals(frameHeader.getDestinationMac(), toObject(nic.getLinkLayerAddresses().get(0).getAddress()))) {
+            return false;
+        }
+
+        // Type debugging log
+        log.debug(Arrays.toString(frameHeader.getType()));
+
+        isInvalidFrameHeaderLessSize(chunk);
         return this.upperLayer.receive(chunk, nic);
     }
 
@@ -25,29 +43,30 @@ public class EthernetLayer extends Layer<FrameHeader> {
         return this.lowerLayer.send(chunk, nic);
     }
 
-    private Byte[] addMacAddress(Byte[] macAddress) {
-        return null;
-    }
+    private FrameHeader extractHeader(Payload payload) {
+        Byte[] payloadArr = payload.getBytes();
+        int totalLength = payloadArr.length;
 
-    private boolean compareMacAddress() {
-        return false;
+        return FrameHeader.builder()
+                .destinationMac(Arrays.copyOfRange(payloadArr, DESTINATION_MAC.getStartIndex(totalLength), DESTINATION_MAC.getEndIndex() + 1))
+                .srcMac(Arrays.copyOfRange(payloadArr, SRC_MAC.getStartIndex(totalLength), SRC_MAC.getEndIndex() + 1))
+                .type(Arrays.copyOfRange(payloadArr, TYPE.getStartIndex(totalLength), TYPE.getEndIndex() + 1))
+                .build();
     }
 
     private void validateIncomingPayloadSize(Payload payload) {
         if(payload.getBytes().length > 1500) throw new IllegalArgumentException("Payload length too large");
     }
 
-    private void checkValidFrameSize(Chunk chunk) {
+    private boolean isInvalidFrameSize(Chunk chunk) {
         int chunkSize = chunk.getPayload().getBytes().length + chunk.getHeader().getBytes().length;
 
-        if(chunkSize > 1514) throw new IllegalArgumentException("Frame size too large");
-        if(chunkSize < 60) throw new IllegalArgumentException("Frame size too small");
+        return chunkSize > 1514 || chunkSize < 60;
     }
 
-    private void checkValidFrameHeaderLessSize(Chunk chunk) {
+    private boolean isInvalidFrameHeaderLessSize(Chunk chunk) {
         int chunkSize = chunk.getPayload().getBytes().length + chunk.getHeader().getBytes().length;
 
-        if(chunkSize > 1500) throw new IllegalArgumentException("FrameHeaderless chunk size too large");
-        if(chunkSize < 46) throw new IllegalArgumentException("FrameHeaderless chunk size too small");
+        return chunkSize > 1500 || chunkSize < 46;
     }
 }
